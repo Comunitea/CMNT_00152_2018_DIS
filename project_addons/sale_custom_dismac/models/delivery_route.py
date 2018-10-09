e58320a81a3b65ce59f3558a623e997118872a57
javierjcf <javierjcf@gmail.com>
2018-10-09 14:28:11 +0200

    [ADD]sale_custom_dismac: Campos cliente para flujo ventasas, zona y ruta reparto

---
+++ project_addons/sale_custom_dismac/models/delivery_route.py
@@ -0,0 +1,11 @@
+# © 2018 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class DeliveryRoute(models.Model):
+
+    _name = 'delivery.route'
+
+    name = fields.Char('Name', required=True)
---
+++ project_addons/sale_custom_dismac/models/sale_order.py
@@ -0,0 +1,32 @@
+# © 2018 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, _
+from odoo.exceptions import UserError
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    @api.multi
+    def action_confirm(self):
+        """
+        Check if order requires client_order_ref
+        """
+        for order in self:
+            if order.partner_id.require_num_order \
+                    and not order.client_order_ref:
+                msg = _('The customer for this order requires a customer \
+                        reference number')
+                raise UserError(msg)
+        res = super(SaleOrder, self).action_confirm()
+        return res
+
+    def onchange_partner_id(self):
+        """
+        Look at the partner for changing the invoice policy
+        """
+        super(SaleOrder, self).onchange_partner_id()
+        if self.partner_id and self.partner_id.whole_orders:
+            self.update({'picking_policy': 'one'})
