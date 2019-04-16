865c44edcab22beb5d64eed74d8ae0919770cf8b
Kiko Sánchez <kiko@comunitea.com>
2019-04-16 13:00:55 +0200

    [DEV] stock_batch_picking_custom

---
+++ project_addons/stock_batch_picking_custom/models/stock_batch_picking.py
@@ -0,0 +1,24 @@
+# Copyright 2019 Comunitea - Kiko Sánchez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from odoo import _, api, fields, models
+
+from odoo.exceptions import ValidationError
+
+
+class StockBatchPicking(models.Model):
+
+    _inherit = 'stock.batch.picking'
+
+    @api.depends('picking_ids.picking_type_id')
+    @api.multi
+    def _get_picking_type(self):
+        for batch in self:
+            batch.picking_type_id = batch.picking_ids and batch.picking_ids[0].picking_type_id or False
+
+    picking_type_id = fields.Many2one('stock.picking.type', compute="_get_picking_type", store=True)
+
+    @api.constrains('picking_ids')
+    def _check_type(self):
+        if len(self.picking_ids.mapped('picking_type_id')) > 1:
+            raise ValidationError(_("All pìckings in batch must be same type"))
+
