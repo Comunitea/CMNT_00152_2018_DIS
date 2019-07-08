0a43f33d8be509cbbddb3479552abaaa1ec065ca
JesusVMayor <jesus@comunitea.com>
2019-07-08 09:16:46 +0200

    [ADD]custom_crm: Añadido módulo.

---
+++ project_addons/custom_crm/__manifest__.py
@@ -0,0 +1,23 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'CRM customizations',
+    'version': '11.0.1.0.0',
+    'category': 'Sales',
+    'author': 'Comunitea',
+    'maintainer': 'Comunitea',
+    'website': 'www.comunitea.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'crm',
+        'product',
+        'stock_account_custom',
+        'product_catalogue_ref',
+    ],
+    'data': [
+        'views/product.xml',
+        'views/res_partner.xml'
+    ],
+    'installable': True,
+}
---
+++ project_addons/custom_crm/models/res_partner.py
@@ -0,0 +1,47 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+from datetime import datetime, timedelta
+
+
+class ResPartner(models.Model):
+
+    _inherit = 'res.partner'
+
+    sales_in_last_six_months = fields.Boolean(
+        compute='_compute_sales_in_last_six_months',
+        search='_search_sales_in_last_six_months')
+
+    def _search_sales_in_last_six_months(self, operator, operand):
+        six_months_ago = datetime.now() + timedelta(days=-180)
+        six_months_ago = six_months_ago.strftime('%Y-%m-%d')
+        sale_order_groups = self.env['sale.order'].read_group(
+            domain=[('date_order', '>=', six_months_ago)],
+            fields=['partner_id'], groupby=['partner_id']
+        )
+        partners = [x['partner_id'][0] for x in sale_order_groups]
+        partner_operator = ''
+        if operator == '=' and operand == True or \
+                operator == '!=' and operand == False:
+            partner_operator = 'in'
+        else:
+            partner_operator = 'not in'
+        return [('id', partner_operator, partners)]
+
+    def _compute_sales_in_last_six_months(self):
+        all_partners = self.search([('id', 'child_of', self.ids)])
+        all_partners.read(['parent_id'])
+        six_months_ago = datetime.now() + timedelta(days=-180)
+        six_months_ago = six_months_ago.strftime('%Y-%m-%d')
+        sale_order_groups = self.env['sale.order'].read_group(
+            domain=[
+                ('partner_id', 'in', all_partners.ids),
+                ('date_order', '>=', six_months_ago)],
+            fields=['partner_id'], groupby=['partner_id']
+        )
+        for group in sale_order_groups:
+            partner = self.browse(group['partner_id'][0])
+            while partner:
+                if partner in self:
+                    partner.sales_in_last_six_months = True
+                partner = partner.parent_id
