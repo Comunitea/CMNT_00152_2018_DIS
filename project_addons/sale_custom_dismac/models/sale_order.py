a521cf49e129214c5cc8d63817342c5044a1d7f5
Roodin <santi@comunitea.com>
2019-07-15 14:38:08 +0200

    [IMP] sale_custom_dismac. Control de la aprobación de condiciones

--- project_addons/sale_custom_dismac/models/sale_order.py
+++ project_addons/sale_custom_dismac/models/sale_order.py
@@ -11,6 +11,8 @@ class SaleOrder(models.Model):
 
     approval_conditions = fields.Boolean('Approval Conditions')
     show_layout = fields.Boolean(related='type_id.show_layout', readonly=True)
+    need_approval = fields.Boolean(related='type_id.need_approval',
+                                   readonly=True)
 
     @api.multi
     def action_confirm(self):
@@ -22,6 +24,10 @@ class SaleOrder(models.Model):
                     and not order.client_order_ref:
                 msg = _('The customer for this order requires a customer \
                         reference number')
+                raise UserError(msg)
+            if order.need_approval \
+                    and not order.approval_conditions:
+                msg = _('This order requires an specific conditions approval')
                 raise UserError(msg)
         res = super().action_confirm()
         return res
--- project_addons/sale_custom_dismac/models/sale_order_type.py
+++ project_addons/sale_custom_dismac/models/sale_order_type.py
@@ -7,4 +7,5 @@ class SaleOrderType(models.Model):
     _inherit = 'sale.order.type'
 
     show_layout = fields.Boolean()
+    need_approval = fields.Boolean(default=False)
     use_partner_agent = fields.Boolean('Use partner commercial')
