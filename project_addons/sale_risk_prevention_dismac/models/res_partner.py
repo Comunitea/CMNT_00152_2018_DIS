87ebe13b67acbaacf6033462ef4c4e7490713897
Roodin <santi@comunitea.com>
2019-07-24 12:27:11 +0200

    [ADD] Añade modulo para prevención  de riesgos

---
+++ project_addons/sale_risk_prevention_dismac/models/res_partner.py
@@ -0,0 +1,29 @@
+#© 2018Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class RiskPreventionDocument(models.Model):
+    _name = 'risk.prevention.document'
+
+    document = fields.Char('Document', required=True)
+    file = fields.Binary(string="File", required=True)
+    due_bool = fields.Boolean("Due", default=False)
+    due_date = fields.Date(string='Due Date')
+    partner_id = fields.Many2one('res.partner')
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    risk_document = fields.Boolean(string='Risk Document needed')
+    risk_document_company_ids = fields.One2many(
+        string='Prevention Company Documents',
+        comodel_name='risk.prevention.document',
+        inverse_name='partner_id',
+    )
+    risk_document_employee_ids = fields.One2many(
+        string='Prevention Employee Documents',
+        comodel_name='risk.prevention.document',
+        inverse_name='partner_id',
+    )
---
+++ project_addons/sale_risk_prevention_dismac/models/sale_order.py
@@ -0,0 +1,42 @@
+# © 2018 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class RiskPreventionDocumentSale(models.Model):
+    _name = 'risk.prevention.document.sale'
+
+    document = fields.Char('Document', required=True)
+    file = fields.Binary(string="File", required=True)
+    sale_order_id = fields.Many2one('sale.order')
+
+
+class SaleOrder(models.Model):
+
+    _inherit = 'sale.order'
+
+    opt_prevention_risk = fields.Boolean(related='type_id.opt_prevention_risk',
+                                   readonly=True)
+    need_prevention_risk = fields.Boolean(string='Need Prevention Risk')
+    request_prevention = fields.Boolean(string='Request prevention risk')
+    prevention_risk_contact_id  = fields.Many2one('res.partner',
+                                                  string='Prevention Contact',
+                                                  domain="[('company_type', " \
+                                                         "'=', 'person')]")
+    installation_explanation= fields.Text('Installation Explanation')
+    subcontracts = fields.Text('Subcontracts')
+    risk_document_ids = fields.One2many(
+        string='Prevention Documents',
+        comodel_name='risk.prevention.document.sale',
+        inverse_name='sale_order_id',
+    )
+    risk_document_completed = fields.Boolean(string='Risk document '
+                                                    'completed', default=False)
+
+
+
+
+
+
