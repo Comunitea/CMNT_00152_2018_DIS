722d8c907ddc97949d32c3142f62e3a119dce79f
JesusVMayor <jesus@comunitea.com>
2019-10-07 12:44:50 +0200

    [MIG]stock_production_lot_text

---
+++ project_addons/stock_production_lot_text/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2018 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Text in production lot',
+    'summary': 'Add Text field to production lot',
+    'version': '12.0.1.0.0',
+    'category': 'Stock',
+    'website': 'comunitea.com',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'stock'
+    ],
+    'data': [
+        'views/stock_production_lot_view.xml',
+    ],
+}
