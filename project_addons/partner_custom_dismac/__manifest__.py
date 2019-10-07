15d214101b0d0a6662bfb8d21e3959b11d80a3e4
JesusVMayor <jesus@comunitea.com>
2019-10-07 11:32:42 +0200

    [MIG]partner_custom_dismac

---
+++ project_addons/partner_custom_dismac/__manifest__.py
@@ -0,0 +1,21 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Partner custom Dismac',
+    'version': '12.0.0.0.1',
+    'summary': 'Allows users to claim ownership over partners after a customizable time.',
+    'category': 'Custom',
+    'author': 'comunitea',
+    'website': 'www.comunitea.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'sale_order_type'
+    ],
+    'data': [
+        'views/sale_order_view.xml',
+        'views/res_partner_view.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+}
