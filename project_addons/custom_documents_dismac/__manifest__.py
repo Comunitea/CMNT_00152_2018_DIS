fcf8c7256b66c1d47b469c5b99a8d93274985e0d
JesusVMayor <jesus@comunitea.com>
2019-10-07 11:06:46 +0200

    [MIG]custom_documents_dismac.

---
+++ project_addons/custom_documents_dismac/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# © 2019 Comunitea -
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    'name': 'Custom Documents Dismac',
+    'version': '11.0.0.0.0',
+    'category': 'Custom',
+    'license': 'AGPL-3',
+    'author': "Comunitea, ",
+    'depends': [
+        'base',
+        'sale',
+        'account',
+        'account_due_dates_str'
+    ],
+    'data': [
+        'views/list_reports.xml',
+        'views/report_templates.xml',
+        'views/stationery_report.xml',
+        # 'views/stationery_iva_report.xml',
+        # 'views/stationery_list_prices_report.xml',
+        # 'views/furniture_report.xml',
+        # 'views/furniture_totals_only_report.xml',
+        'views/invoice_report.xml',
+        # ~ 'views/delivery_note_report.xml',
+        'views/order_report.xml',
+        'views/internal_furniture_report.xml',
+        # 'views/furniture_contests_report.xml',
+        'views/sale_proforma_invoice_report.xml',
+        'views/furniture_new_report.xml',
+        # 'views/furniture_new_iva_report.xml',
+        # 'views/furniture_new_without_photos_report.xml',
+        # 'views/furniture_new_iva_without_photos_report.xml',
+        # 'views/sales_views.xml', No parece necesario
+        # 'views/invoice_report_templates.xml' No parece necesario
+
+    ],
+    'installable': True,
+}
