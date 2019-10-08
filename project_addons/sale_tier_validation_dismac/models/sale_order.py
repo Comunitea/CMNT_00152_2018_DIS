4ea2a6844589e12bda6c304c107a523ebd1cda0b
JesusVMayor <jesus@comunitea.com>
2019-10-08 13:36:39 +0200

    [FIX]sale_tier_validation_dismac: Añadimos campo a los que se permite escribir cuando la orden está validada.

--- project_addons/sale_tier_validation_dismac/models/sale_order.py
+++ project_addons/sale_tier_validation_dismac/models/sale_order.py
@@ -1,7 +1,7 @@
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
 
 
 class SaleOrder(models.Model):
@@ -9,3 +9,9 @@ class SaleOrder(models.Model):
     _inherit = ['sale.order', 'tier.validation']
     _state_from = ['draft', 'sent']
     _state_to = ['sale', 'done']
+
+    @api.model
+    def _get_under_validation_exceptions(self):
+        res = super()._get_under_validation_exceptions()
+        res.append('access_token')
+        return res
