eb86a1439106c4a88827e2ee17df1a786307d9ae
JesusVMayor <jesus@comunitea.com>
2019-10-08 11:55:56 +0200

    [MIG]sale_order_line_form_button.

---
+++ project_addons/sale_order_line_form_button/__manifest__.py
@@ -0,0 +1,26 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale - Button to open sale order lines in form view",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "sale",
+        "sale_stock",
+        "sale_commission"
+    ],
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Daniel Campos <danielcampos@avanzosc.es>"
+    ],
+    "category": "Custom Module",
+    "summary": "",
+    "data": [
+        "views/sale_view.xml",
+    ],
+    "installable": True,
+}
