545ea809bb70e6684fc45bd5ed62b22895bfdbbd
JesusVMayor <jesus@comunitea.com>
2019-10-08 19:20:50 +0200

    [MIG]product_customer_ref.

---
+++ project_addons/product_customer_ref/README.rst
@@ -0,0 +1,29 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=====================
+Product Customer ref
+=====================
+
+This module adds customer ref, like default_code for customer.
+
+
+Configuration
+=============
+
+No configuration is needed.
+
+Usage
+=====
+
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Kiko Sánchez <kiko@comunitea.com>
+
---
+++ project_addons/product_customer_ref/__manifest__.py
@@ -0,0 +1,25 @@
+# © 2018 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Product customer ref',
+    'summary': 'Add customer ref',
+    'version': '12.0.1.0.0',
+    'category': 'Product',
+    'website': 'comunitea.com',
+    'author': 'Comunitea',
+    'license': 'AGPL-3',
+    'application': False,
+    'installable': True,
+    'depends': [
+        'product',
+        'sale',
+        'sale_team_product_custom_dismac'
+
+    ],
+    'data': [
+        'views/product_view.xml',
+        'views/product_customer_value.xml',
+        'views/res_partner.xml',
+        'security/ir.model.access.csv'
+    ],
+}
