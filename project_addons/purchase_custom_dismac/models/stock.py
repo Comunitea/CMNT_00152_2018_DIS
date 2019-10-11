da53889a01d625f82fe890d3b7e6cbe262ae2744
JesusVMayor <jesus@comunitea.com>
2019-10-11 11:13:33 +0200

    Cambiamos merge de account_invoice_report_grouped_by_picking por fork de acysos.

--- project_addons/purchase_custom_dismac/models/stock.py
+++ project_addons/purchase_custom_dismac/models/stock.py
@@ -8,3 +8,13 @@ class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     supplier_ref = fields.Char('Supplier reference', copy=False)
+
+
+class StockMove(models.Model):
+
+    _inherit = 'stock.move'
+
+    def write(self, vals):
+        if vals.get('quantity_done'):
+            import ipdb; ipdb.set_trace()
+        return super().write(vals)
