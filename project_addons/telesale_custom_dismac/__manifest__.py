286acec2e8635dcf496c3dc6348e76b7eb827839
javierjcf <javierjcf@gmail.com>
2019-10-11 10:09:28 +0200

    [MIG]telesale_custom_dismac

---
+++ project_addons/telesale_custom_dismac/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2019 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Telesale Custom Dismac',
+    'version': '12.0.1.0.0',
+    'category': 'Sales',
+    'author': 'Comunitea',
+    'maintainer': 'Comunitea',
+    'website': 'www.comunitea.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'telesale',
+        'sale_custom_dismac',
+    ],
+    'data': [
+        'views/sale_order_type.xml'
+    ],
+    'installable': True,
+}
