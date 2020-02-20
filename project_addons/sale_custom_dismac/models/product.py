e11f33e9212cfffed4bb9ca18076b56ecc80fda4
Kiko Sánchez <kiko@comunitea.com>
2020-02-20 11:58:34 +0100

    [IMP] sale_custom_dismac Vistas y busqueda para product_customerinfo

--- project_addons/sale_custom_dismac/models/product.py
+++ project_addons/sale_custom_dismac/models/product.py
@@ -1,7 +1,16 @@
 # © 2020 Comunitea
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import fields, models, api
 
+
+class ProductCustomerInfo(models.Model):
+    _inherit = "product.customerinfo"
+
+    @api.multi
+    @api.onchange('product_id')
+    def onchange_poroduct_id(self):
+        for line in self:
+            line.product_tmpl_id = line.product_id.product_tmpl_id
 
 class ProductProduct(models.Model):
 
