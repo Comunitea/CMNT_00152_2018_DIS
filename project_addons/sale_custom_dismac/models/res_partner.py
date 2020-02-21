8340c09cd638c3ecc833e24a4c60228bf6714dfd
Roodin <santi@comunitea.com>
2020-02-21 07:38:23 +0100

    [IMP] rest_api_dismac. GEstion de direcciones. Marca de direccion incompleta

--- project_addons/sale_custom_dismac/models/res_partner.py
+++ project_addons/sale_custom_dismac/models/res_partner.py
@@ -17,39 +17,28 @@ class ResPartner(models.Model):
 
 
     def address_get(self, adr_pref=None):
-        # Se cambia solo el to_scan para añadir un sorted, pero tengo que heredear toda la función
-        """ Find contacts/addresses of the right type(s) by doing a depth-first-search
-        through descendants within company boundaries (stop at entities flagged ``is_company``)
-        then continuing the search at the ancestors that are within the same company boundaries.
-        Defaults to partners of type ``'default'`` when the exact type is not found, or to the
-        provided partner itself if no type ``'default'`` is found either. """
+        # Se cambia para ajustar a solicitud del cliente
+        
         adr_pref = set(adr_pref or [])
         if 'contact' not in adr_pref:
             adr_pref.add('contact')
         result = {}
-        visited = set()
         for partner in self:
-            current_partner = partner
-            while current_partner:
-                to_scan = [current_partner]
-                # Scan descendants, DFS
-                while to_scan:
-                    record = to_scan.pop(0)
-                    visited.add(record)
-                    if record.type in adr_pref and not result.get(record.type):
-                        result[record.type] = record.id
-                    if len(result) == len(adr_pref):
-                        return result
-                    to_scan = [c for c in record.child_ids.sorted(
-                        key=lambda r: (r.type and -4 or 0) + (r.default_partner_by_type and -2 or 0) + (
-                                    r.display_name and -1))
-                               if c not in visited
-                               if not c.is_company] + to_scan
 
-                # Continue scanning at ancestor if current_partner is not a commercial entity
-                if current_partner.is_company or not current_partner.parent_id:
-                    break
-                current_partner = current_partner.parent_id
+            for adr in adr_pref:
+                if partner.type == adr:
+                    result[adr] = partner.id
+                else:
+                    default = partner.commercial_partner_id.child_ids.sorted(
+                        lambda r: r.default_partner_by_type).filtered(
+                        lambda p: p.type == adr and p.default_partner_by_type)
+                    if len(default) == 1:
+                        result[adr] = default.id
+                    else:
+                        if default and default[0].default_partner_by_type:
+                            result[adr] = default[0].id
+                        else:
+                            result[adr] = partner.commercial_partner_id.id
 
         # default to type 'contact' or the partner itself
         default = result.get('contact', self.id or False)
