7842c0f7f8d44a775ef01fd61f884891c51c6f8e
Roodin <santi@comunitea.com>
2020-05-07 07:49:28 +0200

    [IMP]website_base.Alguans mejoras en al generacion de definiciones de validacion de web y campo para buiscar partners con usuario web

--- project_addons/website_base/models/res_partner.py
+++ project_addons/website_base/models/res_partner.py
@@ -41,6 +41,34 @@ class ResPartnerAccess(models.Model):
     )
 
     order_validator = fields.Many2one('res.users', string='Orders Validator')
+    global_order_validator = fields.Many2one('res.users', related='commercial_partner_id.order_validator', string='Global Orders Validator')
+    active_portal_user = fields.Boolean('Con usuario web', compute="_compute_active_portal_user", search="_search_active_portal_user")
+
+    def _search_active_portal_user(self, operator, operand):
+        users = self.env['res.users'].search([])
+        partners = users.mapped('partner_id').ids
+        partner_operator = ""
+        if (
+            operator == "="
+            and operand == True
+            or operator == "!="
+            and operand == False
+        ):
+            partner_operator = "in"
+        else:
+            partner_operator = "not in"
+        return [("id", partner_operator, partners)]
+        
+
+    def _compute_active_portal_user(self):
+        users = self.env['res.users'].search([('partner_id', 'in', self.ids)])
+
+        for partner in self:
+            if users.filtered(lambda x: x.partner_id.id == partner.id):
+                partner.active_portal_user =  True 
+            else: 
+                partner.active_portal_user = False
+            
 
     def _get_domain_partner(self):
         if self.website_access_rights == 'own':
@@ -53,29 +81,35 @@ class ResPartnerAccess(models.Model):
 
     @api.onchange('order_validator')
     def _onchange_order_validator(self):
+        
+        sale_model = self.env['ir.model'].search([('model', '=', 'sale.order')])
+        partner_id = self.env['res.partner'].browse(self._origin.id)
+        definition_domain = '["&", ["team_id.team_type", "=", "website"], ["partner_id", "child_of", {}]]'.format(partner_id.id)
+        prev_definitions = self.env['tier.definition'].search([('definition_domain', '=', definition_domain)])
         if self.order_validator:
-            sale_model = self.env['ir.model'].search([('model', '=', 'sale.order')])
-            partner_id = self.env['res.partner'].browse(self._origin.id)
+            print(self.order_validator)
+            values = {
+                'name': _("Validator for partner: {}".format(self.name)),
+                'model_id': sale_model.id,
+                'model': 'sale.order',
+                'review_type': 'individual',
+                'definition_type': 'domain',
+                'active': True,
+                'sequence': 30,
+                'company_id': self.env.user.partner_id.company_id.id,
+                'reviewer_id': self.order_validator.id,
+                'notify_on_create': True,
+                'definition_domain': definition_domain
+            }
 
-            if partner_id:
-                definition_domain = '["&", ["team_id.team_type", "=", "website"], ["partner_id", "child_of", {}]]'.format(partner_id.id)
-                values = {
-                    'name': _("Validator for partner: {}".format(partner_id.name)),
-                    'model_id': sale_model.id,
-                    'model': 'sale.order',
-                    'review_type': 'individual',
-                    'definition_type': 'domain',
-                    'active': True,
-                    'sequence': 30,
-                    'company_id': self.env.user.partner_id.company_id.id,
-                    'reviewer_id': self.order_validator.id,
-                    'notify_on_create': True,
-                    'definition_domain': definition_domain
-                }
-
-                prev_definitions = self.env['tier.definition'].search([('definition_domain', '=', definition_domain)])
+            if prev_definitions:
                 for definition in prev_definitions:
-                    definition.write({
-                        'active': False
-                    })
+                    definition.write(values)
+            else:
                 tier_definition = self.env['tier.definition'].create(values)
+        else:
+            print ("sin validador")
+            if prev_definitions:
+                print ("sin validador, con definicion encontrada")
+                for definition in prev_definitions:
+                    definition.unlink()
