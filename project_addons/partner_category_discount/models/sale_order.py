bf50d4a71c57f1fbcfe57867a95066295128f53d
Santi Argüeso <santi@comunitea.com>
2021-03-09 07:29:16 +0100

    [IMP] dismac_pricing,partner_category_discount,sale_line_history,stock_account_custom: Correciones en precio de ultima compra y precio desde hitorico

--- project_addons/partner_category_discount/models/sale_order.py
+++ project_addons/partner_category_discount/models/sale_order.py
@@ -1,19 +1,31 @@
 # Copyright 2017 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
 
     _inherit = "sale.order.line"
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def product_id_change(self):
         res = super().product_id_change()
         date = self.order_id.date_order or fields.Date.context_today(self)
-        price_and_discount = self.product_id._get_price_and_discount (self.product_uom_qty, self.partner_id, date)
-        self.price_unit = price_and_discount['price']
-        if not self.discount:   
-            self.discount = price_and_discount['discount']
+
+        product = self.product_id.with_context(
+            lang=self.order_id.partner_id.lang,
+            partner=self.order_id.partner_id,
+            quantity=self.product_uom_qty,
+            date=self.order_id.date_order,
+            pricelist=self.order_id.pricelist_id.id,
+            uom=self.product_uom.id,
+        )
+
+        price_and_discount = product._get_price_and_discount(
+            self.product_uom_qty, self.partner_id, date
+        )
+        self.price_unit = price_and_discount["price"]
+        if not self.discount:
+            self.discount = price_and_discount["discount"]
         return res
